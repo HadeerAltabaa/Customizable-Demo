49e8a9a7d1f371a9d5a10bd464da498a68e126ea
TariqJandaly <tariq.jandaly@gmail.com>
2025-08-26 14:33:19 +0300

    Updated auto scripts

--- init.bat
+++ init.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Install live-server globally
-start cmd /k "echo Installing live-server... && npm install -g live-server && echo live-server is installed && pause && exit"
+start cmd /k "echo Installing live-server... && echo This window will close after the installation && npm install -g live-server && echo live-server is installed && exit"
 
 REM Install dependencies in server folder
-start cmd /k "echo Installing dependencies in server folder... && cd server && npm install && echo Dependencies installed && pause && exit"
+start cmd /k "echo Installing dependencies in server folder... && echo This window will close after the installation && cd server && npm install && echo Dependencies installed && exit"
 
 
 exit
